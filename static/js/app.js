b9342ee60f65744b9fc569f5e4633b4766ded995
mugsiemx <mugsiemx@gmail.com>
2023-04-02 03:02:14 -0400

    demographics working, charts no

--- static/js/app.js
+++ static/js/app.js
@@ -12,6 +12,12 @@ const navelId = 1601;
 let data = d3.json(url).then(function(data) {
     // console.log(data);
 
+// create D3 html "variables"
+// to select Navel Id from drop down list
+let navelDropDown = d3.select("#selDataset");
+// to select Navel Id from drop down list
+let demographicsView = d3.select("#sample-metadata");
+
     // create variables for individual navel data
     let names = data.names;
         //console.log(names);
@@ -19,6 +25,8 @@ let data = d3.json(url).then(function(data) {
         //console.log(samples);
     let metadata = data.metadata;
        // console.log(metadata);
+
+
 
     // define all required variables:
     // create filtered data for individual's navel data
@@ -30,7 +38,7 @@ let data = d3.json(url).then(function(data) {
         // console.log(sample_values);
 
         // then title for plots
-    let plotTitle = 'Top 10 Individual Navel Bacterial for Navel Id: 1601'
+    let plotTitle = 'Top 10 Bacteria for Navel Id: 1601'
 
         // then otu_ids as labels for the bar chart
     let otu_ids = filteredSample.otu_ids;
@@ -40,76 +48,93 @@ let data = d3.json(url).then(function(data) {
     let otu_labels = filteredSample.otu_labels;
         // console.log(otu_labels);
 
-            // x axis data to select top 10 (data default is sort desc)
-    let xAxisData = sample_values.slice(0,10);
+            // y axis data to select top 10 (data default is sort desc)
+    let ybarAxisData = otu_ids.slice(0,10).map(otu_id => `OTU ${otu_id}`).reverse();
         // console.log(xAxisData);
 
-            // y axis data to select top 10 (data default is sort desc)
-    let yAxisData = otu_ids.slice(0,10).map(otu_id => `OTU ${otu_id}`).reverse();
+            // x axis data to select top 10 (data default is sort desc)
+    let xbarAxisData = sample_values.slice(0,10);
         // console.log(yAxisData);
 
             // hovertext data to select top 10 (data default is sort desc)
     let hoverText = otu_labels.slice(0,10);
         // console.log(hoverText);
 
-    // create filtered data for individual's demographics    
+    // create filtered data for individual's demographics for the selected Navel Id
     let filteredMetadata = metadata.filter(bacteriaData => bacteriaData.id == navelId)[0];
         // console.log(filteredMetadata);
 
+  
 
-
-
-
-
-    // let selDataset = Object.values(data.names)
-    // console.log(selDataset)
+// 2. create horizontal bar chart to display the top 10 OTUs for individual's navels
+    // trace for the navel data
+    // console.log(ybarAxisData);
+    console.log(xbarAxisData);
+    let traceBar = [{
+        x: {xbarAxisData},
+        y: {ybarAxisData},
+        text: hoverText,
+        type: "bar",
+        orientation: "h"
+    }];
     
-    // dropdown list option not selected, make blank
-    // let option = "";
+    // Create data array
+    let barData = [traceBar];
 
-    // // add values to form drop down list
-    // for(let i =0;i<selDataset.length;i++)
-    // {
-    // option += '<option value="'+ selDataset[i] +'">' + selDataset[i] + "</option>"
-    // }
-
-
-
-
-
-
-// 2. create horizontal bar chart to display the top 10 OTUs for individualS
-
-        // trace for the navel data
-        let trace = [{
-            x: xAxisData,
-            y: yAxisData,
-            text: hoverText,
-            type: "bar",
-            orientation: "h"
-        }];
-        
-        // Create data array
-        let barData = [trace];
-        
-        // Apply a title to the layout
-        let barLayout = {
-            title: plotTitle,
-            // Include margins in the layout so the x-tick labels display correctly
-            margin: {
-            l: 50,
-            r: 50,
-            b: 200,
-            t: 50,
-            pad: 4
-            }
-        };
-        
-        // Render the plot to the div tag with id "plot"
-        Plotly.newPlot("bar", barData, barLayout);
+    // Apply a title to the layout
+    let barLayout = {
+        title: plotTitle,
+        // Include margins in the layout so the x-tick labels display correctly
+        margin: {
+        l: 50,
+        r: 50,
+        b: 200,
+        t: 50,
+        pad: 4
+        }
+    };
+    
+    // Render the plot to the div tag with id "plot"
+    Plotly.newPlot("bar", barData, barLayout);
 
 // 3. create bubble chart that displays each sample
-// 4. display the sample metadata, i.e., an individual's demographic info
+    // trace for the navel data
+    let traceBubble = [{
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: 'markers',
+        type: "bubble",
+        marker:{
+            size: [sample_values],
+            color: otu_ids,
+            colorscale: 'Hot'
+        }        
+    }];
+    
+    // Create data array
+    let bubbleData = [traceBubble];
+
+    var bubbleLayout = {
+        title: plotTitle,
+        showlegend: false,
+        height: 600,
+        width: 600
+      };
+
+    // Render the plot to the div tag with id "plot"
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+// 4. display the sample metadata, i.e., an individual's demographic info    
 // 5. Display each key-value pair from the metadata JSON object somewhere on the page.
+    Object.entries(filteredMetadata).forEach(entry => {
+    let [key, value] = entry;
+   // console.log(key, value);
+        demographicsView.append("p").text(`${key}: ${value}`);
+  });
 });
 // 6. update all the plots when a new sample is selected
+    // function updatePlotly(newId) {
+    //     Plotly.restyle("bar", "values", [newdata]);
+        // Plotly.restyle("bubble", "values", [newdata]);
+    // }
